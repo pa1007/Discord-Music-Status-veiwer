e0db2cac91bae7a62a47f6468e413d7cc86e5d69
pa1007 <pa.fourriere@sfr.fr>
2019-01-07 21:40:01 +0100

    Remove unused method

--- src/main/java/fr/pa1007/song/User32.java
+++ src/main/java/fr/pa1007/song/User32.java
@@ -8,21 +8,11 @@ import com.sun.jna.win32.StdCallLibrary;
 
 public interface User32 extends StdCallLibrary {
 
-    User32 INSTANCE = (User32) Native.loadLibrary("user32", User32.class);
-
-    WinDef.HWND GetForegroundWindow();  // add this
-
+    User32 INSTANCE = Native.loadLibrary("user32", User32.class);
 
     boolean EnumWindows(WinUser.WNDENUMPROC lpEnumFunc, Pointer arg);
 
     int GetWindowTextA(WinDef.HWND hWnd, byte[] lpString, int nMaxCount);
 
-    WinDef.HWND FindWindowA(String winClass, String title);
-
-    WinDef.HWND FindWindowExA(WinDef.HWND hwndParent, WinDef.HWND childAfter, String className, String windowName);
-
-    WinDef.HWND FindWindowExW(String winClass, String title);
-
-    WinDef.HWND FindWindowW(String winClass, String title);
 
 }
